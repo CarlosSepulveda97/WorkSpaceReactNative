91f705da045f6dc1771cd6b94fccf5ecbf0d7ecc
Carlos Sepulveda <carlos.sepulvedahe@gmail.com>
2020-08-25 22:37:57 -0400

    Boton submit funcional

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Dimensions, Button } from 'react-native';
 
-export default function App() {
+const width = Dimensions.get('window').width
+
+export default function App() {//Es como el constructor de la vista 
+  const [text, setText] = useState('Formulario')
+  const [submit, setSubmit] = useState('')
+  const titulo= 'Formulario'
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{titulo}</Text>
+      <TextInput 
+        style = {styles.input} 
+        placeholder = 'Escribe aqui'
+        onChangeText = {t => setText(t)}
+        defaultValue = {''}
+      /> 
+      <Button onPress={() => {
+        setSubmit(text)
+        alert('Se guardo lo siguiente: '+ text)
+      }} title="Aceptar"/>
+      <Text>{submit}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input:{
+    height: 40,
+    borderBottomColor:'#ccc',
+    borderBottomWidth: 1,
+    width: '100%',//width: width
+  },
+  
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+
